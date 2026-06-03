771d8a11a450e9063e297f149ca6c19153f88640
rojadandu123 <rojadandu13@gmail.com>
2026-06-03 12:51:31 +0530

    Add feature branch update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
     <div>
       <h1>Welcome to My React Application</h1>
       <p>Created using React and GitHub CLI.</p>
+      <p>Feature branch update for pull request demo.</p>
     </div>
   );
 }
